cfb8dca0c0a6138ea4fc1107789c2c572b7cdea4
spikol <ds@di.ku.dk>
2023-12-20 19:50:50 +0100

    normalization

--- Cirkusnaturligvis/data_note/normal_stdev.py
+++ Cirkusnaturligvis/data_note/normal_stdev.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import itertools
 
-# Function to read and normalize data
+# Function to read and Z-score normalize data
 def read_and_normalize(file_path):
     df = pd.read_csv(file_path)
     # Assuming the last two columns are non-numeric (labels, etc.), and the rest are the data to be normalized
     data_columns = df.columns[:-2]
-    # Normalize such that each row sums to 1
-    df_normalized = df[data_columns].div(df[data_columns].sum(axis=1), axis=0)
+    # Normalize using Z-score: (X - mean) / std
+    df_normalized = df[data_columns].apply(lambda x: (x - x.mean()) / x.std(), axis=0)
     return df_normalized
 
 # File paths
@@ -23,7 +22,7 @@ average_responses = [df.mean() for df in normalized_data]
 # Extracting wavelength values from the columns (assuming all files have the same wavelengths)
 wavelengths = normalized_data[0].columns
 
-# Creating a DataFrame for easier plotting
+# Creating a DataFrame for easier plotting and analysis
 average_responses_df = pd.DataFrame(average_responses, index=['samples_06', 'samples_13', 'samples_29'])
 average_responses_df = average_responses_df.T  # Transpose for easier plotting
 
@@ -33,7 +32,7 @@ plt.figure(figsize=(12, 6))
 for sample_id in average_responses_df.columns:
     plt.plot(wavelengths, average_responses_df[sample_id], marker='o', label=sample_id)
 
-plt.title('Average Normalized Response over Wavelength')
+plt.title('Average Normalized Response over Wavelength (Z-score normalization)')
 plt.xlabel('Wavelength (nm)')
 plt.ylabel('Normalized Response')
 plt.legend(title='Sample Set')
@@ -43,7 +42,5 @@ plt.grid(True)
 plt.show()
 
 # Printing the table of average responses
-print("Table of Average Normalized Responses:")
+print("Table of Average Normalized Responses (Z-score normalization):")
 print(average_responses_df)
-
-
